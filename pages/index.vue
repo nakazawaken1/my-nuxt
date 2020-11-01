d3c77bd29b2bdb9a676300bc13c0b360f77deb59
nakazawaken1 <nakazawaken1@gmail.com>
2020-11-01 07:25:58 +0000

    add remove

--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,20 @@
   <div>
     <h1>Todo リスト</h1>
     <table>
-      <tr><th>連番</th><th>内容</th><th>完了</th><th>追加日時</th></tr>
+      <tr><th>連番</th><th>内容</th><th>完了</th><th>追加日時</th><th>操作</th></tr>
       <tr v-for="(todo, i) in todos" :key="i">
         <td>{{i + 1}}</td>
         <td>{{todo.title}}</td>
         <td><input type="checkbox" :checked="todo.done"/></td>
         <td>{{format(todo.created_at)}}</td>
+        <td><button @click.prevent="remove(i)">削除</button></td>
       </tr>
       <tr>
         <td>新規</td>
         <td><input type="text" v-model="title"/></td>
         <td></td>
-        <td><button>追加</button></td>
+        <td></td>
+        <td><button @click.prevent="add">追加</button></td>
       </tr>
     </table>
     {{title}}
@@ -38,6 +40,19 @@ export default {
         + '/' + date.getDate()
         + ' ' + date.getHours()
         + ':' + date.getMinutes();
+    },
+    add() {
+      if(this.title) {
+        this.todos.push({
+          title: this.title,
+          done: false,
+          created_at: new Date()
+        });
+        this.title = null;
+      }
+    },
+    remove(i) {
+      this.todos.splice(i, 1);
     }
   }
 }
